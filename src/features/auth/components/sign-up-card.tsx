49801997ee50b1ae6279001a5fa2785d7b7e3539
Han Myat Thu <hanmyatthu111@gmail.com>
2024-10-17 15:17:12 +0200

    create ui for auth screens

---
+++ src/features/auth/components/sign-up-card.tsx
@@ -0,0 +1,97 @@
+"use client";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { DottedSeparator } from "@/components/common/dotted-separator";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export const SignUpCard = () => {
+  return (
+    <Card className="w-full h-full md:w-[486px] border-none shadow-sm">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          By Signing up, you agree to our{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Terms of service</span>
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            type="text"
+            name="name"
+            required
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter name"
+            disabled={false}
+          />
+          <Input
+            type="email"
+            name="email"
+            required
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter email address"
+            disabled={false}
+          />
+          <Input
+            type="password"
+            name="password"
+            required
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter password"
+            disabled={false}
+            min={8}
+            max={256}
+          />
+          <Button disabled={false} size="lg" className="w-full">
+            Sign up
+          </Button>
+        </form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="w-full"
+          disabled={false}
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          className="w-full"
+          disabled={false}
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with Gitub
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
