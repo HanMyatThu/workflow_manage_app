052af3ed8eb1d4257c1b10393666885e2b0419ef
Han Myat Thu <hanmyatthu111@gmail.com>
2024-10-28 13:47:16 +0100

    create sidebar component

---
+++ src/components/sidebar/navigation.tsx
@@ -0,0 +1,38 @@
+import { SettingsIcon, UsersIcon } from "lucide-react";
+import {
+  GoCheckCircle,
+  GoCheckCircleFill,
+  GoHome,
+  GoHomeFill,
+} from "react-icons/go";
+
+const routes = [
+  {
+    label: "Home",
+    href: "/",
+    icon: GoHome,
+    activeIcon: GoHomeFill,
+  },
+  {
+    label: "My Tasks",
+    href: "/tasks",
+    icon: GoCheckCircle,
+    active: GoCheckCircleFill,
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: SettingsIcon,
+    active: SettingsIcon,
+  },
+  {
+    label: "Members",
+    href: "/members",
+    icon: UsersIcon,
+    active: UsersIcon,
+  },
+];
+
+export const Navigation = () => {
+  return <div>Hello</div>;
+};
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+
+import { DottedSeparator } from "@/components/common/dotted-separator";
+import { Navigation } from "./navigation";
+
 export const Sidebar = () => {
-  return <aside></aside>;
+  return (
+    <aside className="h-full bg-neutral-200 w-full p-4">
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={164} height={50} />
+      </Link>
+      <DottedSeparator />
+      <Navigation />
+    </aside>
+  );
 };
